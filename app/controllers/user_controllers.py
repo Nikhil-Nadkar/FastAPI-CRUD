ddebcb50c29448176fd4f4e58325c2c3ccf664aa
Nikhil Nadkar <nikhilnadkar127@gmail.com>
2026-08-13 23:28:14 +0530

    Complete CRUD, HTTPException & Error Handling

--- app/controllers/user_controllers.py
+++ app/controllers/user_controllers.py
@@ -1,6 +1,67 @@
 from app.models.user import User
 from sqlalchemy.orm import Session
+from app.schemas.user import UserCreate
+from fastapi import HTTPException
+from sqlalchemy.exc import IntegrityError
 
-def get_users(db:Session):
+
+def get_users(db: Session):
     user = db.query(User).all()
-    return {'message' : "got all users", 'users': user}
+
+    return {"message": "got all users", "users": user}
+
+
+def create_user(user: UserCreate, db: Session):
+    newuser = User(name=user.name, email=user.email)
+
+    try:
+        db.add(newuser)
+        db.commit()
+        db.refresh(newuser)
+
+        return newuser
+
+    except IntegrityError:
+        db.rollback()
+
+        raise HTTPException(status_code=422, detail="Email already exists")
+
+
+def get_single_user(user_id: int, db: Session):
+    user = db.query(User).filter(User.id == user_id).first()
+
+    if not user:
+        raise HTTPException(status_code=404, detail="user not found")
+
+    return user
+
+
+def update_user(user: UserCreate, user_id: int, db: Session):
+    getuser = db.query(User).filter(User.id == user_id).first()
+
+    if not getuser:
+        raise HTTPException(status_code=404, detail={"user not found"})
+
+    getuser.name = user.name
+    getuser.email = user.email
+
+    try:
+        db.commit()
+        db.refresh(getuser)
+
+        return getuser
+    except IntegrityError:
+        db.rollback()
+
+        raise HTTPException(status_code=422, detail="Email already exists")
+
+
+def delete_user(user_id: int, db: Session):
+    user = db.query(User).filter(user_id == User.id).first()
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    db.delete(user)
+    db.commit()
+    return {"message": "user deleted", "user_id": user_id}
--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,12 +1,38 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from app.controllers.user_controllers import get_users
+from app.controllers.user_controllers import (
+    get_users,
+    create_user,
+    get_single_user,
+    delete_user,
+    update_user,
+)
 from app.database.database import get_db
+from app.schemas.user import UserCreate
 
 router = APIRouter()
 
-@router.get('/')
-def get_all_users(
-  db: Session = Depends(get_db)
-):
-  return get_users(db)
+
+@router.get("/")
+def get_all_users(db: Session = Depends(get_db)):
+    return get_users(db)
+
+
+@router.post("/")
+def create_user_route(user: UserCreate, db: Session = Depends(get_db)):
+    return create_user(user, db)
+
+
+@router.get("/{user_id}")
+def get_single_user_routes(user_id: int, db: Session = Depends(get_db)):
+    return get_single_user(user_id, db)
+
+
+@router.delete("/{user_id}")
+def delete_user_routes(user_id: int, db: Session = Depends(get_db)):
+    return delete_user(user_id, db)
+
+
+@router.put("/{user_id}")
+def update_user_routes(user: UserCreate, user_id: int, db: Session = Depends(get_db)):
+    return update_user(user, user_id, db)
